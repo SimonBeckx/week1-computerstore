995f60f1935bb021df61c8af9b2e44e1fd55ed95
Simon <Simon@DESKTOP-4I7GFI9>
2017-09-26 19:39:09 +0200

    Added code ( update 8 )

---
+++ src/be/pxl/computerstore/hardware/ComputerSystem.java
@@ -0,0 +1,33 @@
+package be.pxl.computerstore.hardware;
+
+public class ComputerSystem {
+	
+	private Processor processor;
+	private ComputerCase computerCase;
+	
+	public ComputerSystem() {
+	}
+
+	public Processor getProcessor() {
+		return processor;
+	}
+
+	public void setProcessor(Processor processor) {
+		this.processor = processor;
+	}
+
+	public ComputerCase getComputerCase() {
+		return computerCase;
+	}
+
+	public void setComputerCase(ComputerCase computerCase) {
+		this.computerCase = computerCase;
+	}
+	
+	public void addPheripheral(Peripheral pheripheral) {
+		
+	}
+	
+	
+
+}
